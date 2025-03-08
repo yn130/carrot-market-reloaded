c5d32e208bae0159df615f70e5cfce59a5c0f157
yn130 <tjdpdms95@gmail.com>
2025-03-08 23:56:01 +0900

    4.1 Authentication UI

--- app/create-account/page.tsx
+++ app/create-account/page.tsx
@@ -7,7 +7,7 @@ export default function CreateAccount(){
     return (
         <div className="flex flex-col gap-10 py-8 px-6">
             <div className="flex flex-col gap-2 *:font-medium">
-                <h1 className="text-2xl">HELLO</h1>
+                <h1 className="text-2xl">HELLO!</h1>
                 <h2 className="text-xl">Fill in the form below to join!</h2>
             </div>
             <form className="flex flex-col gap-3">
