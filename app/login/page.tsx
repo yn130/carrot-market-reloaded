5bf6d5e9500d6c83cf814d8e7eca8862f83ab602
yn130 <tjdpdms95@gmail.com>
2025-03-09 16:27:26 +0900

    5.1 Server Actions

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,24 +1,35 @@
 
-// import FormButton from "@/components/form-btn";
-// import FormInput from "@/components/form-input";
-// import SocialLogin from "@/components/social-login";
+import FormButton from "@/components/form-btn";
+import FormInput from "@/components/form-input";
+import SocialLogin from "@/components/social-login";
 
-// export default function LogIn(){
-//     return (
-//         <div className="flex flex-col gap-10 py-8 px-6">
-//             <div className="flex flex-col gap-2 *:font-medium">
-//                 <h1 className="text-2xl">HELLO!</h1>
-//                 <h2 className="text-xl">Log in with email and password.</h2>
-//             </div>
-//             <form className="flex flex-col gap-3">
-//                 <FormInput type="email" placeholder="Email" required errors={[]}/>
-//                 <FormInput type="password" placeholder="Password" required errors={[]}/>
-//                 <FormButton loading={false} text="Login"/>
-//             </form>
-//             <SocialLogin />
-//         </div>
-//     )
-// }
+export default function LogIn(){
+    // const handlerForm = async () => {
+    //     "use server";
+    // console.log("i run in the server baby!")
+    // }
+    async function handlerForm(data:FormData) {
+        "use server";
+        console.log(data);
+        console.log("i run in the server baby!")
+
+    }
+
+    return (
+        <div className="flex flex-col gap-10 px-6 py-8">
+            <div className="flex flex-col gap-2 *:font-medium">
+                <h1 className="text-2xl">HELLO!</h1>
+                <h2 className="text-xl">Log in with email and password.</h2>
+            </div>
+            <form action={handlerForm} className="flex flex-col gap-3">
+                <FormInput name="email" type="email" placeholder="Email" required errors={[]}/>
+                <FormInput name="password" type="password" placeholder="Password" required errors={[]}/>
+                <FormButton loading={false} text="Login"/>
+            </form>
+            <SocialLogin />
+        </div>
+    )
+}
 
 
 
@@ -26,38 +37,40 @@
 
 // ---------------------------------------------------------- //
 
-"use client";
+// Route Handler 사용 
 
-import FormButton from "@/components/form-btn";
-import FormInput from "@/components/form-input";
-import SocialLogin from "@/components/social-login";
+// "use client";
 
-export default function LogIn(){
-    const onClick = async () => {
-        const response = await fetch("/api/users", {
-            method:"POST",
-            body: JSON.stringify({
-                username:"yeeun",
-                password:"1234"
-            })
-        })
-        console.log(await response.json());
-    }
+// import FormButton from "@/components/form-btn";
+// import FormInput from "@/components/form-input";
+// import SocialLogin from "@/components/social-login";
 
-    return (
-        <div className="flex flex-col gap-10 py-8 px-6">
-            <div className="flex flex-col gap-2 *:font-medium">
-                <h1 className="text-2xl">HELLO!</h1>
-                <h2 className="text-xl">Log in with email and password.</h2>
-            </div>
-            <form className="flex flex-col gap-3">
-                <FormInput type="email" placeholder="Email" required errors={[]}/>
-                <FormInput type="password" placeholder="Password" required errors={[]}/>
-            </form>
-               <span onClick={onClick}>
-                    <FormButton loading={false} text="Login"/>
-                </span>
-            <SocialLogin />
-        </div>
-    )
-}
+// export default function LogIn(){
+//     const onClick = async () => {
+//         const response = await fetch("/api/users", {
+//             method:"POST",
+//             body: JSON.stringify({
+//                 username:"yeeun",
+//                 password:"1234"
+//             })
+//         })
+//         console.log(await response.json());
+//     }
+
+//     return (
+//         <div className="flex flex-col gap-10 px-6 py-8">
+//             <div className="flex flex-col gap-2 *:font-medium">
+//                 <h1 className="text-2xl">HELLO!</h1>
+//                 <h2 className="text-xl">Log in with email and password.</h2>
+//             </div>
+//             <form className="flex flex-col gap-3">
+//                 <FormInput type="email" placeholder="Email" required errors={[]}/>
+//                 <FormInput type="password" placeholder="Password" required errors={[]}/>
+//             </form>
+//                <span onClick={onClick}>
+//                     <FormButton loading={false} text="Login"/>
+//                 </span>
+//             <SocialLogin />
+//         </div>
+//     )
+// }
--- components/form-input.tsx
+++ components/form-input.tsx
@@ -3,6 +3,7 @@ interface FormInputProps {
     placeholder: string;
     required: boolean;
     errors: string[];
+    name:string;
   }
   
   export default function FormInput({
@@ -10,17 +11,19 @@ interface FormInputProps {
     placeholder,
     required,
     errors,
+    name,
   }: FormInputProps) {
     return (
       <div className="flex flex-col gap-2">
         <input
-          className="bg-transparent rounded-md w-full h-10 focus:outline-none ring-2 focus:ring-4 transition ring-neutral-200 focus:ring-orange-500 border-none placeholder:text-neutral-400"
+          name={name}
+          className="w-full h-10 transition bg-transparent border-none rounded-md focus:outline-none ring-2 focus:ring-4 ring-neutral-200 focus:ring-orange-500 placeholder:text-neutral-400"
           type={type}
           placeholder={placeholder}
           required={required}
         />
         {errors.map((error, index) => (
-          <span key={index} className="text-red-500 font-medium">
+          <span key={index} className="font-medium text-red-500">
             {error}
           </span>
         ))}
