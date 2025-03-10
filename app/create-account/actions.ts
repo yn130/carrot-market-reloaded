706aec1de635307bd003e4ca9a5c1047f25554af
yn130 <tjdpdms95@gmail.com>
2025-03-10 16:15:22 +0900

    6.1 Validation Error

--- app/create-account/actions.ts
+++ app/create-account/actions.ts
@@ -1,9 +1,12 @@
 "use server"
 import {z} from "zod";
 
-const usernameSchema = z.string().min(5).max(10);
-
-
+const formSchema = z.object({
+    username: z.string().min(3).max(10),
+    email: z.string().email(),
+    password: z.string().min(10),
+    confirmPassword: z.string().min(10),
+})
 
 export async function createAccount(prevState:any, date:FormData) {
 
@@ -12,9 +15,16 @@ export async function createAccount(prevState:any, date:FormData) {
         email: date.get("email"),
         password: date.get("password"),
         comfirmPassword: date.get("comfirmPassword"),
+    };
+    // try {
+    //     formSchema.parse(data)
+    // } catch (e) {
+    //     console.log(e);
+    // }
+
+    const result = formSchema.safeParse(data);
+    if(!result.success){
+        console.log(result.error.flatten);
+        return result.error.flatten();
     }
-    console.log(data)
-
-    usernameSchema.parse(data.username)
-
 }
