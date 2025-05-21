5314dd4436806e80393784e5e5a6d56809ebca9f
yn130 <tjdpdms95@gmail.com>
2025-05-21 17:34:03 +0900

    6.3 Transformation

--- app/create-account/actions.ts
+++ app/create-account/actions.ts
@@ -1,6 +1,9 @@
 "use server"
 import {z} from "zod";
 
+const passwordRegex = new RegExp(
+/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[#?!@$%^&*-]).+$/
+);
 function checkUsername(username:string){
     return !username.includes("potato");
 }
@@ -15,13 +18,17 @@ const formSchema = z.object({
         required_error: "Where is your username?",
     })
     .min(3, "Way too short!!")
-    .max(10, "That is too long!!!")
+    .max(20, "That is too long!!!")
+    .toLowerCase() //대문자 자동으로 소문자로 전환
+    .trim() //공백 자동으로 제거
+    // .transform((username) => username.replace(/ /g, "")), //공백 자동으로 제거
+    .transform((username) => `🎾 ${username} 🎾`) // 문자열 앞뒤에 문자열 추가
     // .refine((username) => username.includes("potato") ? false:true,"No potatos allowed!"), 
     // .refine((username) => !username.includes
     .refine(checkUsername,"No potatos allowed!"),
-    email: z.string().email(),
-    password: z.string().min(10),
-    confirmPassword: z.string().min(10),
+    email: z.string().email().toLowerCase(),
+    password: z.string().min(4).regex(passwordRegex, "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character"),
+    confirmPassword: z.string().min(4).regex(passwordRegex),
 })
 .refine(checkPassword, {
     message: "Both Passwords must be the same!",
@@ -42,9 +49,12 @@ export async function createAccount(prevState:any, date:FormData) {
     //     console.log(e);
     // }
 
-    const result = formSchema.safeParse(data);
+    // 유효성 검사
+    const result = formSchema.safeParse(data); // safeParse(data): 데이터 유효성 검사를 시도
     if(!result.success){
-        console.log(result.error.flatten());
+        console.log(result.error.flatten()); // result.error.flatten()으로 보기 쉬운 오류 메시지를 뽑아서 리턴
         return result.error.flatten();
+    } else {
+        console.log(result.data);
     }
 } 
